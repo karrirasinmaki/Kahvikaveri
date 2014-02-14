3100dccaa4614712f80f50bf6070add40311ace0
karrirasinmaki <karri.rasinmaki@gmail.com>
2014-02-14 03:59:57 +0200

    Code coverage - at least! Also little UI and DB upgrades

--- coffeebuddy/coffeebuddy/src/main/java/fi/raka/coffeebuddy/logic/Tag.java
+++ coffeebuddy/coffeebuddy/src/main/java/fi/raka/coffeebuddy/logic/Tag.java
@@ -6,6 +6,8 @@ package fi.raka.coffeebuddy.logic;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import fi.raka.coffeebuddy.storage.ReceiptDatabaseHelper;
 import fi.raka.coffeebuddy.storage.Saveable;
 import fi.raka.coffeebuddy.storage.ReceiptContract.ReceiptEntry;
 
@@ -47,8 +49,8 @@ public class Tag implements Saveable {
 
 	@Override
 	public Integer saveToDB(Context context) {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteDatabase db = new ReceiptDatabaseHelper(context).getWritableDatabase();
+		return ReceiptDatabaseHelper.Utils.saveToDB(db, ReceiptEntry.TAGS_TABLE_NAME, getValues(), getReferenceId());
 	}
 
 	@Override
